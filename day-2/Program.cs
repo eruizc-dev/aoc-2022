6987893c64d7c44403f2de58ac4f17880bfec9fa
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-12-03 23:29:16 -0300

    Day 2, part 2

--- day-2/Program.cs
+++ day-2/Program.cs
@@ -1,23 +1,72 @@
-﻿int GetResultScore(int opponents, int mine) {
-	var diff = opponents - mine;
-	if (diff == 0) {
-		return 3;
+﻿var lines = System.IO.File.ReadAllLines("./input.txt");
+var p1 = Part1.GetScore(lines);
+Console.WriteLine(p1);
+var p2 = Part2.GetScore(lines);
+Console.WriteLine(p2);
+
+public static class Part1 {
+	public static int GetScore(string[] lines) {
+		var total = 0;
+		foreach (var line in lines) {
+			total += GetMatchScore(line[0] - 'A', line[2] - 'X');
+		}
+		return total;
 	}
-	if (diff == -1 || diff == 2) {
-		return 6;
+
+	private static int GetMatchScore(int opponents, int mine) {
+		var shapeScore = mine + 1;
+		var resultScore = GetResultScore(opponents, mine);
+		return shapeScore + resultScore;
 	}
-	return 0;
+
+	private static int GetResultScore(int opponents, int mine) {
+		var diff = opponents - mine;
+		if (diff == 0) {
+			return 3;
+		}
+		if (diff == -1 || diff == 2) {
+			return 6;
+		}
+		return 0;
+	}
 }
 
-int GetScore(int opponents, int mine) {
-	var shapeScore = mine + 1;
-	var resultScore = GetResultScore(opponents, mine);
-	return shapeScore + resultScore;
-}
+public static class Part2 {
+	public static int GetScore(string[] lines) {
+		var total = 0;
+		foreach (var line in lines) {
+			total += GetMatchScore(line[0] - 'A', line[2]);
+		}
+		return total;
+	}
 
-var total = 0;
-var lines = System.IO.File.ReadAllLines("./input.txt");
-foreach (var line in lines) {
-	total += GetScore(line[0] - 'A', line[2] - 'X');
+	private static int GetMatchScore(int opponents, char result) {
+		var mine = GetMyOption(opponents, result);
+		var shapeScore = mine + 1;
+		var resultScore = GetResultScore(opponents, mine);
+		return shapeScore + resultScore;
+	}
+
+	private static int GetResultScore(int opponents, int mine) {
+		var diff = opponents - mine;
+		if (diff == 0) {
+			return 3;
+		}
+		if (diff == -1 || diff == 2) {
+			return 6;
+		}
+		return 0;
+	}
+
+	private static int GetMyOption(int opponents, char result) {
+		if (result == 'Y') {
+			return opponents;
+		}
+
+		if (result == 'X') {
+			return (opponents + 2) % 3;
+		}
+
+		return (opponents + 1) % 3;
+	}
 }
-Console.WriteLine(total);
