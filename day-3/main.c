c631b1bf9af6be550762ea8048f8b293e66c3b2f
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-12-07 21:50:02 -0300

    Better day 3

--- day-3/main.c
+++ day-3/main.c
@@ -28,23 +28,25 @@ uint64_t get_bit_array(const char *str, int len) {
         uint64_t exponent = str[i] < 'a'
             ? str[i] - 'A' + 'z' - 'a' + 1
             : str[i] - 'a';
-        printf("%c: %lu - %d\n", str[i], exponent, len);
         array |= powd(2, exponent);
     }
     return array;
 }
 
-uint64_t get_elf_priority_value(const char *sack_1, const char *sack_2, int len) {
-    uint64_t arr_1 = get_bit_array(sack_1, len);
-    uint64_t arr_2 = get_bit_array(sack_2, len);
+uint64_t get_elf_priority_value(const char* elf) {
+    int sack_len = strlen(elf) / 2;
+    const char* sack_1 = elf;
+    const char* sack_2 = elf + sack_len;
+    uint64_t arr_1 = get_bit_array(sack_1, sack_len);
+    uint64_t arr_2 = get_bit_array(sack_2, sack_len);
     uint64_t arr_common = arr_1 & arr_2;
     return get_value(arr_common);
 }
 
-uint64_t get_group_priority_value(const char *elf_1, const char *elf_2, const char *elf_3) {
-    uint64_t arr_1 = get_bit_array(elf_1, strlen(elf_1) - 1);
-    uint64_t arr_2 = get_bit_array(elf_2, strlen(elf_2) - 1);
-    uint64_t arr_3 = get_bit_array(elf_3, strlen(elf_3) - 1);
+uint64_t get_group_priority_value(char* elfs[3]) {
+    uint64_t arr_1 = get_bit_array(elfs[0], strlen(elfs[0]) - 1);
+    uint64_t arr_2 = get_bit_array(elfs[1], strlen(elfs[1]) - 1);
+    uint64_t arr_3 = get_bit_array(elfs[2], strlen(elfs[2]) - 1);
     uint64_t arr_common = arr_1 & arr_2 & arr_3;
     return get_value(arr_common);
 }
@@ -63,12 +65,11 @@ int main(int argc, char* argv[]) {
     char elf_1[100], elf_2[100], elf_3[100];
     char *group[] = { elf_1, elf_2, elf_3 };
     while (fgets(group[elfs], 100, input)) {
-        char* elf = group[elfs];
-        int sack_len = strlen(elf) / 2;
-        score_1 += get_elf_priority_value(elf, elf + sack_len, sack_len);
+        score_1 += get_elf_priority_value(group[elfs]);
+        elfs++;
 
-        if (++elfs == 3) {
-            score_2 += get_group_priority_value(group[0], group[1], group[2]);
+        if (elfs == 3) {
+            score_2 += get_group_priority_value(group);
             elfs = 0;
         }
     }
